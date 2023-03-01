a2b54650d4b9fc357de46582537e72f80d06fd04
Artem-Podoliak <podoliak.a.s@gmail.com>
2023-03-01 14:29:53 +0200

    123

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector(".gallery");
+const addGalleryItems = createGalleryItems(galleryItems);
+
+galleryRef.insertAdjacentHTML("beforeend", addGalleryItems);
+
+function createGalleryItems(elements) {
+  return elements
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`;
+    })
+    .join("");
+}
+
+const options = {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+};
+
+const lightbox = new SimpleLightbox(".gallery a", options);
