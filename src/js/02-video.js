af1d9599e719a3cff3e32bb5ae187dd4afba1a05
Artem-Podoliak <podoliak.a.s@gmail.com>
2023-03-02 16:22:04 +0200

    66666666

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const iframePlayer = new Player(iframe);
+const localStorageKey = `videoplayer-current-time`;
+let saveTime = localStorage.getItem(localStorageKey);
+
+if (saveTime) {
+  iframePlayer.setCurrentTime(saveTime);
+}
+iframePlayer.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem(localStorageKey, data.seconds);
+  }),
+  1000
+);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const localStorageKey = `feedback-form-state`;
+
+form.addEventListener('input', throttle(saveValue, 500));
+form.addEventListener('submit', submitForm);
+autocompleteFormIsLocal();
+
+function saveValue(params) {
+  const { email, message } = params.currentTarget.elements;
+
+  const inputValue = {
+    email: email.value,
+    message: message.value,
+  };
+
+  const inputValueJson = JSON.stringify(inputValue);
+  localStorage.setItem(localStorageKey, inputValueJson);
+}
+
+function submitForm(params) {
+  params.preventDefault();
+  params.currentTarget.reset();
+  localStorage.removeItem(localStorageKey);
+}
+
+function autocompleteFormIsLocal(params) {
+  const inputValueToLocal = localStorage.getItem(localStorageKey);
+  const inputValueToLocalRevers = JSON.parse(inputValueToLocal);
+  if (inputValueToLocalRevers) {
+    form.email.value = inputValueToLocalRevers.email;
+    form.message.value = inputValueToLocalRevers.message;
+  }
+}
